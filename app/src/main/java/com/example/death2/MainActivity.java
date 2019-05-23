d618484921b2734e633e81be5543930f1f98fc2b
Subhradeep Saha <subhradeep11012000@gmail.com>
2019-05-23 14:14:44 +0530

    Updated background settings

--- app/src/main/java/com/example/death2/MainActivity.java
+++ app/src/main/java/com/example/death2/MainActivity.java
@@ -7,13 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    public float a=0;
+    public float b=0;
     public int w=0;
     public int l=0;
     public EditText x;
@@ -31,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
         compare=(TextView)findViewById(R.id.compare);
         x=(EditText)findViewById(R.id.no);
 
-
     }
+
     public void compare(View v)
     {
         String ageStr=age.getText().toString();
@@ -47,21 +48,74 @@ public class MainActivity extends AppCompatActivity {
     }
     private void displayresult(float agevalue,float guessvalue,float xvalue) {
         String label = "";
-        a++;
-        if(Float.compare(a,xvalue)<=0) {
+        LinearLayout t;
+        t=(LinearLayout)findViewById((R.id.activity_main));
+        float a=Math.abs(agevalue-guessvalue);
+        b++;
+        if((a<=100&&a>90))
+            t.setBackgroundColor(Color.parseColor("#f44336"));
+        else if((a<=90&&a>80))
+            t.setBackgroundColor(Color.parseColor("#ef5350"));
+        else if((a<=80&&a>70))
+            t.setBackgroundColor(Color.parseColor("#e57373"));
+        else if((a<=70&&a>60))
+            t.setBackgroundColor(Color.parseColor("#ffcdd2"));
+        else if((a<=60&&a>50))
+            t.setBackgroundColor(Color.parseColor("#ffebee"));
+        else if((a<=50&&a>40))
+            t.setBackgroundColor(Color.parseColor("#c8e6c9"));
+        else if((a<=40&&a>30))
+            t.setBackgroundColor(Color.parseColor("#aed581"));
+        else if((a<=30&&a>20))
+            t.setBackgroundColor(Color.parseColor("#8bc34a"));
+        else if((a<=20&&a>10))
+            t.setBackgroundColor(Color.parseColor("#66bb6a"));
+        else if((a<=10&&a>0))
+            t.setBackgroundColor(Color.parseColor("#8bc34a"));
+        else
+            t.setBackgroundColor(Color.parseColor("#4caf50"));
+        if(Float.compare(b,xvalue)<0) {
             if (Float.compare(guessvalue, agevalue) < 0)
             {
                 label = getString(R.string.less);
-                l++;}
+                l++;
+
+            }
             else if (Float.compare(guessvalue, agevalue) > 0){
                 label = getString(R.string.more);
-                l++;}
+                l++;
+
+            }
             else{
                 label = getString(R.string.exact);
                 w++;}
+
+            if((a<=100&&a>90))
+                t.setBackgroundColor(Color.parseColor("#f44336"));
+            else if((a<=90&&a>80))
+                t.setBackgroundColor(Color.parseColor("#ef5350"));
+            else if((a<=80&&a>70))
+                t.setBackgroundColor(Color.parseColor("#e57373"));
+            else if((a<=70&&a>60))
+                t.setBackgroundColor(Color.parseColor("#ffcdd2"));
+            else if((a<=60&&a>50))
+                t.setBackgroundColor(Color.parseColor("#ffebee"));
+            else if((a<=50&&a>40))
+                t.setBackgroundColor(Color.parseColor("#c8e6c9"));
+            else if((a<=40&&a>30))
+                t.setBackgroundColor(Color.parseColor("#aed581"));
+            else if((a<=30&&a>20))
+                t.setBackgroundColor(Color.parseColor("#8bc34a"));
+            else if((a<=20&&a>10))
+                t.setBackgroundColor(Color.parseColor("#66bb6a"));
+            else if((a<=10&&a>0))
+                t.setBackgroundColor(Color.parseColor("#8bc34a"));
+            else
+                t.setBackgroundColor(Color.parseColor("#4caf50"));
         }
-        else
+        else{
             label=getString(R.string.lost);
+        t.setBackgroundColor(Color.parseColor("#bdbdbd"));}
         String s="Success :";
         String f="Fail :";
 
